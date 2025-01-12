14fca0452a095f769a2c63c7538c649ca92ebcd9
jinno <klvx01@gmail.com>
2025-01-12 11:18:15 +0900

    fix: update install script for pygraphviz

--- install_12.sh
+++ install_12.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# This is for zoltraak_auto
+# This is for pygraphviz
+sudo apt install -y python3-dev graphviz libgraphviz-dev pkg-config
+sudo pip install pygraphviz
 
+# This is for zoltraak_auto
 eval "$(pyenv init -)"
 cd zoltraak_auto
 pip install -e .
